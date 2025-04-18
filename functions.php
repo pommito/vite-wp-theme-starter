e6096dc988156105257f595dda514b162d599039
Victor Lebecq <victor.lebecq.pro@gmail.com>
2025-04-18 17:16:04 +0200

    create a dev mode and env mode for assets

--- functions.php
+++ functions.php
@@ -1,20 +1,40 @@
 <?php
 
 add_action("wp_enqueue_scripts", function () {
-    $manifestPath = get_theme_file_path("dist/.vite/manifest.json");
+    if (defined("WP_DEBUG") && WP_DEBUG) {
+        // DEV ENV
+        $viteDevServer = "http://localhost:3000";
+        wp_enqueue_script(
+            "vite-client",
+            $viteDevServer . "/@vite/client",
+            [],
+            null,
+            true
+        );
+        wp_enqueue_script(
+            "theme",
+            $viteDevServer . "/assets/js/main.js",
+            [],
+            null,
+            true
+        );
+    } else {
+        // PROD ENV
+        $manifestPath = get_theme_file_path("dist/.vite/manifest.json");
 
-    if (file_exists($manifestPath)) {
-        $manifest = json_decode(file_get_contents($manifestPath), true);
+        if (file_exists($manifestPath)) {
+            $manifest = json_decode(file_get_contents($manifestPath), true);
 
-        if (isset($manifest["assets/js/main.js"])) {
-            $mainJs = $manifest["assets/js/main.js"]["file"];
-            wp_enqueue_script(
-                "theme",
-                get_theme_file_uri("dist/" . $mainJs),
-                [],
-                null,
-                true
-            );
+            if (isset($manifest["assets/js/main.js"])) {
+                $mainJs = $manifest["assets/js/main.js"]["file"];
+                wp_enqueue_script(
+                    "theme",
+                    get_theme_file_uri("dist/" . $mainJs),
+                    [],
+                    null,
+                    true
+                );
+            }
         }
     }
 });
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,25 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  server: {
+    host: "0.0.0.0",
+    port: 3000,
+    hmr: {
+      host: "localhost",
+      port: 3000,
+    },
+    proxy: {
+      "/wp-content/themes/theme-starter-wp": {
+        target: "http://wp-starter.local", // Remplacez par l'URL de votre site Local
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+    watch: {
+      usePolling: true,
+      interval: 1000,
+    },
+  },
   build: {
     outDir: "dist",
     rollupOptions: {
